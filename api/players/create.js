dc266420c5c34bbcebd875745b8c67ad35c3b81a
Bobby Galli <bobbyg603@gmail.com>
2017-09-14 12:26:50 -0400

    Added get and create.

--- api/players/create.js
+++ api/players/create.js
@@ -1,16 +1,37 @@
 'use strict';
 
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
 module.exports.create = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'create new player',
-      input: event,
-    }),
+  const timestamp = new Date().getTime();
+  if (!event.username) {
+    console.error('Validation Failed');
+    callback(new Error('Couldn\'t create the player, invalid username.'));
+    return;
+  }
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      username: event.username,
+      password: event.password,
+      createdAt: timestamp,
+      updatedAt: timestamp,
+    },
   };
+  
+  dynamoDb.put(params, (error) => {
+    if (error) {
+      console.error(error);
+      callback(new Error('Couldn\'t create the todo item.'));
+      return;
+    }
 
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+    callback(null, response);
+  });
 };
--- api/players/get.js
+++ api/players/get.js
@@ -1,16 +1,26 @@
 'use strict';
 
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
 module.exports.get = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'get a player',
-      input: event,
-    }),
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      username: event.pathParameters.username,
+    },
   };
 
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  dynamoDb.get(params, (error, result) => {
+    if (error) {
+      console.error(error);
+      callback(new Error('Couldn\'t fetch the todo item.'));
+      return;
+    }
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Item),
+    };
+    callback(null, response);
+  });
 };
