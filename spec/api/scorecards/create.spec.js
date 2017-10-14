3b200c1519513cfaddb166c6f28952c2be365e0c
Bobby Galli <bobbyg603@gmail.com>
2017-10-14 17:40:04 -0400

    Added sample model for testing creation of scorecards.

---
+++ spec/api/scorecards/create.spec.js
@@ -0,0 +1,167 @@
+// TODO BG
+// {
+//     "course": {
+//       "city": "Canterbury",
+//       "holes": [
+//         {
+//           "par": 3,
+//           "number": 1,
+//           "description": "Short par 3 with a dangerous hill from right to left. This has a moderate tree coverage and is ripe for some good fuggin shots.",
+//           "distance": 189
+//         },
+//         {
+//           "par": 3,
+//           "number": 2,
+//           "description": "Long par 3 that banks right at the end. The lane is decent and opens up if you can make it to the top of the hill. \"Step up and get the potatoes ready because you will have to mash your drive\".",
+//           "distance": 228
+//         },
+//         {
+//           "par": 4,
+//           "number": 3,
+//           "description": "Long par 4 that requires power and accuracy to hit position A and rendere a good approach. Keep it on line because the woods can gobble discs on both the left and right.",
+//           "distance": 484
+//         },
+//         {
+//           "par": 4,
+//           "number": 4,
+//           "description": "Slightly up hill with a couple landing zones. You will need 2 really good shots to get up to the pin for a birdie opportunity.",
+//           "distance": 432
+//         },
+//         {
+//           "par": 3,
+//           "number": 5,
+//           "description": "Longer down hill par 3 with a lot of space toward the pin.",
+//           "distance": 283
+//         },
+//         {
+//           "par": 3,
+//           "number": 6,
+//           "description": "Good par 3 the bends from left to right.",
+//           "distance": 257
+//         },
+//         {
+//           "par": 4,
+//           "number": 7,
+//           "description": "Long par 4 that requires a lot of shot shaping. Giddy up.",
+//           "distance": 447
+//         },
+//         {
+//           "par": 4,
+//           "number": 8,
+//           "description": "Long slightly down hill par 4 with a big lane. Eagle it bro!",
+//           "distance": 418
+//         },
+//         {
+//           "par": 3,
+//           "number": 9,
+//           "description": "Short up hill par 3 that marks the end of the front 9. Chugga chugga choo choo all aboard the birdie train!",
+//           "distance": 230
+//         },
+//         {
+//           "par": 3,
+//           "number": 10,
+//           "description": "Short flat par 3 with a slight left bend. Play a skip and catch a bird dude.",
+//           "distance": 231
+//         },
+//         {
+//           "par": 3,
+//           "number": 11,
+//           "description": "Short down hill par 3 with a mandatory lane. Get them down there make bird guy.",
+//           "distance": 208
+//         },
+//         {
+//           "par": 3,
+//           "number": 12,
+//           "description": "Step into the open and bombs away!",
+//           "distance": 304
+//         },
+//         {
+//           "par": 3,
+//           "number": 13,
+//           "description": "Long tough par 3 across the power lines and down the hill to the pin. Will make or break your round.",
+//           "distance": 379
+//         },
+//         {
+//           "par": 3,
+//           "number": 14,
+//           "description": "Medium up hill par three that plays long because of the elevation gain. The hole opens up at the top.",
+//           "distance": 303
+//         },
+//         {
+//           "par": 3,
+//           "number": 15,
+//           "description": "Very birdie-able. Style points for hyzer-bombs!",
+//           "distance": 314
+//         },
+//         {
+//           "par": 5,
+//           "number": 16,
+//           "description": "Awesome long snaking par 5 through the woods. Keep it on the lane and rider her up there!",
+//           "distance": 565
+//         },
+//         {
+//           "par": 4,
+//           "number": 17,
+//           "description": "Short par 4 that's a good chance to gain a stroke on the field. Drink a beer about it!",
+//           "distance": 376
+//         },
+//         {
+//           "par": 4,
+//           "number": 18,
+//           "description": "Long par 4 that takes you back to the clubhouse. A 3 is a solid score and a 2 is downright heroic.",
+//           "distance": 498
+//         }
+//       ],
+//       "zip": "03224",
+//       "updatedAt": 1506127550930,
+//       "createdAt": 1506127550930,
+//       "name": "Top of the Hill",
+//       "street": "68 Southwest Rd",
+//       "state": "NH"
+//     },
+//     "scores": [
+//       {
+//         "players": [
+//           "Kyle"
+//         ],
+//         "scores": [
+//           {
+//             "hole": 1,
+//             "score": "1"
+//           },
+//           {
+//             "hole": 2,
+//             "score": "1"
+//           },
+//           {
+//             "hole": 3,
+//             "score": "1"
+//           },
+//           {
+//             "hole": 4,
+//             "score": "1"
+//           },
+//           {
+//             "hole": 5,
+//             "score": "1"
+//           },
+//           {
+//             "hole": 6,
+//             "score": "1"
+//           },
+//           {
+//             "hole": 7,
+//             "score": "1"
+//           },
+//           {
+//             "hole": 8,
+//             "score": "1"
+//           },
+//           {
+//             "hole": 9,
+//             "score": "1"
+//           }
+//         ]
+//       }
+//     ]
+//   }
