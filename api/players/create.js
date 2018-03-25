c3b2c2ec28955afdbe794add3861cc0fdf0c3e15
bobbyg603 <bobbyg603@gmail.com>
2018-03-25 16:17:28 -0400

    Hash passwords with bcrypt and use bcrypt for validation.

--- api/players/create.js
+++ api/players/create.js
@@ -2,66 +2,84 @@
 
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const bcrypt = require("bcryptjs");
+const saltRounds = 10;
 
 module.exports.create = (event, context, callback) => {
   const timestamp = new Date().getTime();
   const data = JSON.parse(event.body);
   const dynamoItem = {};
-  
-  if (data.username) {
-    dynamoItem.username = data.username;
-  } else {
+
+  if (!data.username) {
     console.error('Validation Failed');
     callback(new Error('Couldn\'t create the player, invalid username.'));
     return;
   }
 
-  if (data.password) {
-    dynamoItem.password = data.password;
-  } else {
+  if (!data.password) {
     console.error('Validation Failed');
     callback(new Error('Couldn\'t create the player, invalid password.'));
     return;
+
   }
 
-  if (data.firstName) {
-    dynamoItem.firstName = data.firstName;
-  }
+  bcrypt.genSalt(saltRounds)
+    .then(salt => bcrypt.hash(data.password, salt))
+    .then(hashedPassword => {
+      dynamoItem.username = data.username;
+      dynamoItem.password = hashedPassword;
 
-  if (data.lastName) {
-    dynamoItem.lastName = data.lastName;
-  }
+      if (data.firstName) {
+        dynamoItem.firstName = data.firstName;
+      }
 
-  if (data.email) {
-    dynamoItem.email = data.email;
-  }
-  
-  if (data.phone) {
-    dynamoItem.phone = data.phone;
-  }
+      if (data.lastName) {
+        dynamoItem.lastName = data.lastName;
+      }
 
-  dynamoItem.updatedAt = timestamp;
-  dynamoItem.createdAt = timestamp;
+      if (data.email) {
+        dynamoItem.email = data.email;
+      }
 
-  const params = {
-    TableName: process.env.DYNAMODB_TABLE,
-    Item: dynamoItem
-  };
-  
-  dynamoDb.put(params, (error) => {
-    if (error) {
+      if (data.phone) {
+        dynamoItem.phone = data.phone;
+      }
+
+      dynamoItem.updatedAt = timestamp;
+      dynamoItem.createdAt = timestamp;
+
+      const params = {
+        TableName: process.env.DYNAMODB_TABLE,
+        Item: dynamoItem
+      };
+
+      dynamoDb.put(params, (error) => {
+        if (error) {
+          console.error(error);
+          callback(new Error('Couldn\'t create the player.'));
+          return;
+        }
+
+        delete params.Item.password;
+        const response = {
+          statusCode: 200,
+          headers: {
+            "Access-Control-Allow-Origin": "*"
+          },
+          body: JSON.stringify(params.Item),
+        };
+        callback(null, response);
+      });
+    })
+    .catch(error => {
       console.error(error);
-      callback(new Error('Couldn\'t create the player.'));
-      return;
-    }
-
-    const response = {
-      statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*"
-      },
-      body: JSON.stringify(params.Item),
-    };
-    callback(null, response);
-  });
+      const response = {
+        statusCode: 500,
+        headers: {
+          "Access-Control-Allow-Origin": "*"
+        },
+        body: "Internal Server Error",
+      };
+      callback(null, response);
+    });
 };
